a3d8d9da2a9c195eb694fe5676099e0733c84ca2
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-11-25 22:01:50 +0300

    hw13.1 ready

--- src/test/java/ru/netology/ProductTest.java
+++ src/test/java/ru/netology/ProductTest.java
@@ -10,6 +10,8 @@ class ProductTest {
         ProductManager manager = new ProductManager(repo);
         Product product1 = new Book(1, "Книга 1", 500, "Автор1");
         Product product2 = new Smartphone(2, "Смартфон 1", 10_000, "производитель1");
+        Product product3 = new Book(3, "Книга 2", 1_000, "Автор1");
+
 
 
     @Test
@@ -70,7 +72,39 @@ class ProductTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+    @Test
 
+    public void searchNull() {
+
+        manager.add(product1);
+        manager.add(product2);
+
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Автор10");
+
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void searchTwoResult() {
+
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+
+        Product[] expected = {product1, product3};
+        Product[] actual = manager.searchBy("Автор1");
+
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
     @Test
 
     public void searchVendor() {
