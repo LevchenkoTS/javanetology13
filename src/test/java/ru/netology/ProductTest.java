6e8221db3447181727b687ffc6ad9d407f46b497
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-11-30 22:22:54 +0300

    hw 13.1 corr

--- src/test/java/ru/netology/ProductTest.java
+++ src/test/java/ru/netology/ProductTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 class ProductTest {
 
 
-        ProductRepository repo = new ProductRepository();
-        ProductManager manager = new ProductManager(repo);
-        Product product1 = new Book(1, "Книга 1", 500, "Автор1");
-        Product product2 = new Smartphone(2, "Смартфон 1", 10_000, "производитель1");
+    ProductRepository repo = new ProductRepository();
+    ProductManager manager = new ProductManager(repo);
+    Product product1 = new Book(1, "Книга 1", 500, "Автор1");
+    Product product2 = new Smartphone(2, "Смартфон 1", 10_000, "производитель1");
+    Product product3 = new Book(3, "Книга 1", 1_000, "Автор2");
 
 
     @Test
@@ -32,6 +33,7 @@ class ProductTest {
 
         manager.add(product1);
         manager.add(product2);
+        manager.add(product3);
 
 
         Product[] expected = {product1};
@@ -47,12 +49,47 @@ class ProductTest {
 
         manager.add(product1);
         manager.add(product2);
+        manager.add(product3);
 
         repo.removeById(product2.getId());
 
-        Product [] expected = {product1};
+        Product[] expected = {product1, product3};
         Product[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+
+    public void searchNull() {
+
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Книга10");
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void searchTwoResult() {
+
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+
+        Product[] expected = {product1, product3};
+        Product[] actual = manager.searchBy("Книга 1");
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+        }
+
 }
